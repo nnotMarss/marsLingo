4047d3c37638c7e3159576132ea3609d10dec506
Fuzzy <cassidy.off@outlook.com.au>
2023-11-03 21:47:32 +0000

    Snapshot 23w44a! :*

--- langMars.py
+++ langMars.py
@@ -2,14 +2,35 @@ class EN:
     class StartSC:
         WelcMSG = "Welcome to TerraLingo!"
         SubWelcMSG = "Here you'll learn how to use Terranian, and how to write & read it's characters! Easy!"
-    pass
+        DscWelcMSG = "Please choose one out of these modes: [1] Characters Learning; [2] Full Sentences Learning (Coming Soon!); [3] Translator (Coming Soon!); [4] News? (Coming Soon!);"
+    class SelectorSC:
+        WrongAws = "Oops, looks like there's no option like this.."
+        WrongAwsSub = "Try again: [1] Characters Learning; [2] Full Sentences Learning (Coming Soon!); [3] Translator (Coming Soon!); [4] News? (Coming Soon!);"
+        WrongAwsValue = "Oh-uh! Looks like this awser is NOT an integer (a number), this awser MUST BE a integer!! D:"
+    class AlphaSC:
+        AlphaMSG = "Looks like you found an unfinished page! Sorry!"
+    class QuizSC:
+        RightMSG = "Horray! You got it correct! %s makes a %s sound! :D\n  Wait 3 sec to continue training, or press [CTRL]+[C] to quit!"
+        WrongMSG = "Oops! You got it wrong.. %s makes a %s sound :3\n  Wait 3 sec to continue training, or press [CTRL]+[C] to quit!"
 class PL:
     class StartSC:
         WelcMSG = "Witamy w TerraLingo!"
-        SubWelcMSG = "Tutaj nauczysz sie jak używac Terranian-a, i jak pisać & czytać pismo! Łatwizna!"
-    pass
+        SubWelcMSG = "Tutaj nauczysz sie jak używac Terranian-a, i jak pisać & czytać symbole! Łatwizna!"
+        DscWelcMSG = ""
+    class SelectorSC:
+        WrongAws = ""
+        WrongAwsSub = ""
+        WrongAwsValue = ""
+    class AlphaSC:
+        AlphaMSG = "Looks like you found an unfinished page! Sorry! Check if english has a finished page!"
 class RU:
     class StartSC:
         WelcMSG = "Привет в ТерраЛинго!"
         SubWelcMSG = "Здесь вы узнаете, как использовать Тэрраниян, а также как писать и читать его символы! Легкий!"
-    pass
+        DscWelcMSG = ""
+    class SelectorSC:
+        WrongAws = ""
+        WrongAwsSub = ""
+        WrongAwsValue = ""
+    class AlphaSC:
+        AlphaMSG = "This language is coming in V1! Or in one of the V0 Snapshots!"
