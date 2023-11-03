984d0a43c63c5175fa5a845895d640ffc8f202df
Fuzzy <cassidy.off@outlook.com.au>
2023-11-03 19:27:13 +0000

    initial commit!! :33

---
+++ langMars.py
@@ -0,0 +1,15 @@
+class EN:
+    class StartSC:
+        WelcMSG = "Welcome to TerraLingo!"
+        SubWelcMSG = "Here you'll learn how to use Terranian, and how to write & read it's characters! Easy!"
+    pass
+class PL:
+    class StartSC:
+        WelcMSG = "Witamy w TerraLingo!"
+        SubWelcMSG = "Tutaj nauczysz sie jak używac Terranian-a, i jak pisać & czytać pismo! Łatwizna!"
+    pass
+class RU:
+    class StartSC:
+        WelcMSG = "Привет в ТерраЛинго!"
+        SubWelcMSG = "Здесь вы узнаете, как использовать Тэрраниян, а также как писать и читать его символы! Легкий!"
+    pass
